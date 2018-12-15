d91f8ad92d4b4250945067d9ed2fe4a1b5b90531
Björn Bebensee <bjoern.bebensee@rwth-aachen.de>
2018-12-15 03:46:44 +0100

    Parse html with BeautifulSoup and extract all song titles

--- extract.py
+++ extract.py
@@ -1,5 +1,5 @@
 import sys
-import urllib
+import urllib.request
 from bs4 import BeautifulSoup
 
 def extract(url):
@@ -11,4 +11,10 @@ for arg in sys.argv[1:]:
 	urls.append(arg)
 
 for url in urls:
-	print("URL: " + url)
+	print("Downloading " + url)
+	with urllib.request.urlopen(url) as response:
+		html = response.read()
+	page = BeautifulSoup(html, features="html.parser")
+	titles = page.body.findAll('span', attrs={'class' : 'tracklist-item__text__headline'})
+	for title in titles:
+		print(title.text)
